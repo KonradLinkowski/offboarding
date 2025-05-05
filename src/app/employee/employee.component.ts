a5df66f4dc2f40bc03b1832b27355d0f587d7b57
Konrad Linkowski <26126510+KonradLinkowski@users.noreply.github.com>
2025-05-05 15:33:36 +0200

    Employee component styling

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,16 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { MatDivider } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { OffboardingDialog } from './offboarding/offboarding.dialog';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
-import { map, Observable } from 'rxjs';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { map } from 'rxjs';
 import { Employee } from '../types';
 import { EmployeesService } from '../employees.service';
 
 @Component({
   selector: 'app-employee',
-  imports: [MatDivider],
+  imports: [MatTableModule, MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.scss',
 })
